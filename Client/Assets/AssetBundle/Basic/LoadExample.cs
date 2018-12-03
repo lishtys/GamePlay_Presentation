ab834ef72c817e2bd978e92a8c87ae1405b23ae3
ChenMi <lishtys@gmail.com>
2018-12-03 08:04:26 -0700

    Update Basic Demo AB

---
+++ Client/Assets/AssetBundle/Basic/LoadExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LoadExample : MonoBehaviour {
+
+    void Start()
+    {
+        var myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "unitychan.eae"));
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+
+        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("UnityChan");
+        Instantiate(prefab);
+
+        myLoadedAssetBundle.Unload(false);
+    }
+}
