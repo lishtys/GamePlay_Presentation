3a9c45cab920816e0ee1f91de012d41eb374b612
Lishtys <lishtys@gmail.com>
2018-12-03 09:43:20 -0700

    Add All Loading Functions

--- Client/Assets/AssetBundle/Basic/LoadExample.cs
+++ Client/Assets/AssetBundle/Basic/LoadExample.cs
@@ -2,21 +2,68 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class LoadExample : MonoBehaviour {
 
-    void Start()
+    IEnumerator Start()
     {
-        var myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "unitychan.eae"));
-        if (myLoadedAssetBundle == null)
-        {
-            Debug.Log("Failed to load AssetBundle!");
-            return;
-        }
+        var path = Path.Combine(Application.streamingAssetsPath, "unitychan.eae");
 
-        var prefab = myLoadedAssetBundle.LoadAsset<GameObject>("UnityChan");
+        #region Load From File
+        //        var ab = AssetBundle.LoadFromFile(path);
+        //        if (ab == null)
+        //        {
+        //            Debug.Log("Failed to load AssetBundle!");
+        //            yield return null;
+        //        } 
+        #endregion
+
+
+        #region Load From Memeory
+        //        AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(path));
+        //        yield return request;
+        //        var ab = request.assetBundle;
+
+
+
+        //         var ab = AssetBundle.LoadFromMemory(File.ReadAllBytes(path)); 
+        #endregion
+
+
+        #region WWW
+
+        //        while(Caching.ready == false)
+        //        {
+        //            yield return null;
+        //        }
+        //
+        //       
+        //       WWW www = WWW.LoadFromCacheOrDownload(path, 1);
+        //        yield return www;
+        //        if ( string.IsNullOrEmpty(www.error)==false )
+        //        {
+        //            Debug.Log(www.error);
+        //            yield break ;
+        //        }
+        //        AssetBundle ab = www.assetBundle;
+
+        #endregion
+
+        #region WebRequest
+
+        UnityWebRequest request = UnityWebRequest.GetAssetBundle(path);
+        yield return request.Send();
+        AssetBundle ab = DownloadHandlerAssetBundle.GetContent(request); 
+        #endregion
+
+
+        //Load Content
+        var prefab = ab.LoadAsset<GameObject>("UnityChan");
         Instantiate(prefab);
 
-        myLoadedAssetBundle.Unload(false);
+        ab.Unload(false);
+        yield return null;
+
     }
 }
